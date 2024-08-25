dc9f2e76759105408309e13699d4f1b4fed08345
Juan Chaher <juanchaher99@gmail.com>
2024-08-25 20:01:27 -0300

    first client commit

---
+++ client/src/app/providers.js
@@ -0,0 +1,8 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+// import { SessionProvider } from "next-auth/react";
+
+export function Providers({ children }) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
