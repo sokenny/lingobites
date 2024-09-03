34224122626e6c2c9e7195e6fa879888dfd8e490
Juan Chaher <juanchaher99@gmail.com>
2024-09-03 20:44:20 -0300

    new allowed origins

--- server/app.js
+++ server/app.js
@@ -26,8 +26,20 @@ export const openai = new OpenAI({
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://lingobites.io",
+  "https://www.lingobites.io",
+];
+
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
